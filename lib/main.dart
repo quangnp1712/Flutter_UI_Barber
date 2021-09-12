4459363c06f5a823135a1b7fffb617bd518f7e6b
DESKTOP-2NBON1N\ASUS <quangnp1712@gmail.com>
2021-09-12 21:55:03 +0700

    update image

--- lib/main.dart
+++ lib/main.dart
@@ -20,53 +20,56 @@ class _Register extends State<MyApp>{
           constraints: BoxConstraints.expand(),
           color: Colors.white, // background
           width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end, // căn từ dưới lên
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 40),
-                child: Container(
-                  width: 70,
-                    height: 70,
-                    padding: EdgeInsets.all(10),
-                    child: FlutterLogo()),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: Text(
-                  "Đăng ký tài khoản mới", style: TextStyle(
-                  fontWeight: FontWeight.bold, color: Colors.black, fontSize: 30
-                ),),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: Column(
-                  children: <Widget>[
-                    inputFile(label:"Họ và Tên" , hintText: "Enter your fullname"),
-                    inputFile(label:"Số điện thoại" , hintText: "Enter your phone number"),
-                    inputFile(label:"Tên đăng nhập" , hintText: "Enter username"),
-                    inputFile(label:"Mật khẩu" , hintText: "Enter password", obscureText: true),
-                    inputFile(label:"Xác nhận mật khẩu" , hintText: "Enter confirm password", obscureText: true),
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end, // căn từ dưới lên
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Container(
+                    width: 120,
+                      height: 120,
+                      padding: EdgeInsets.all(0),
+                      child: Image.asset('assets/barber.png')
+                       ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 56,
-                  child: RaisedButton(
-                    color: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8))
-                    ),
-                    onPressed: onSignInClicked,
-                    child: Text("Đăng ký ngay",
-                    style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: Text(
+                    "CREATE YOUR ACCOUNT", style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25
+                  ),),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label:"FullName" , hintText: "Enter your fullname"),
+                      inputFile(label:"Phone Number" , hintText: "Enter your phone number"),
+                      inputFile(label:"UserName" , hintText: "Enter username"),
+                      inputFile(label:"Password" , hintText: "Enter password", obscureText: true),
+                      inputFile(label:"Confirm Password" , hintText: "Enter confirm password", obscureText: true),
+                    ],
                   ),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: RaisedButton(
+                      color: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))
+                      ),
+                      onPressed: onSignInClicked,
+                      child: Text("Đăng ký ngay",
+                      style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),)
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       )
